c19cc2e9a3241660f756a38dae2615dae374c21a
sirflyingv <andthenyouadd@gmail.com>
2022-08-08 14:01:35 +0300

    fix

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 import elementColorFlicker from '/elementColorFlicker.js';
-import { INTERVAL } from './config.js';
-import { COLOR_DELTA_MAX } from './config.js';
+import { COLOR_DELTA_MAX, INTERVAL } from './config.js';
 
 const list = document.querySelectorAll('.list__element');
 
